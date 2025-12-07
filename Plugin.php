0c631092996e3cb06c5f02badd184abf66cadd91
regularjogger <160317134+regularjogger@users.noreply.github.com>
2025-12-07 22:04:06 +0100

    Fix form element names

--- Plugin.php
+++ Plugin.php
@@ -41,18 +41,18 @@ class Plugin extends PluginBase
      */
     public function detectBots(array &$post, object $component): void
     {
-        if (! empty($post['web']) || ! array_key_exists('web-url', $post) || ! empty($post['web-url'])) {
+        if (! empty($post['web']) || ! array_key_exists('web_url', $post) || ! empty($post['web_url'])) {
             $post['HONEYPOT_web'] = $post['web'];
             unset($post['web']);
             $post['HONEYPOT_TIMED_web_url'] = $post['web_url'];
-            unset($post['web-url']);
+            unset($post['web_url']);
             Log::info('Magic Forms submission dismissed.' . PHP_EOL . PHP_EOL . 'Form alias/name: ' . $component->alias . '/' . $component->name . PHP_EOL . PHP_EOL . print_r($post, true));
             $component->setProperty('mail_enabled', 0);
             $component->setProperty('mail_resp_enabled', 0);
             $component->setProperty('skip_database', 1);
         }
         unset($post['web']);
-        unset($post['web-url']);
+        unset($post['web_url']);
     }
 
     /**
